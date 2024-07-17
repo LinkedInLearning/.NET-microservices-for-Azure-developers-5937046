ce916c4ee752fe27302dfaf85d99c0bba90925d7
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-07-17 15:35:39 -0700

    02_06e

--- src/Wpm.Management.Api/Controllers/PetsController.cs
+++ src/Wpm.Management.Api/Controllers/PetsController.cs
@@ -15,12 +15,30 @@ public class PetsController(ManagementDbContext dbContext) : ControllerBase
         return Ok(all);
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("{id}", Name = nameof(GetById))]
     public async Task<IActionResult> GetById(int id)
     {
         var pet = await dbContext.Pets.Include(p => p.Breed)
             .Where(p => p.Id == id)
             .FirstOrDefaultAsync();
         return Ok(pet);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(NewPet newPet)
+    {
+        var pet = newPet.ToPet();
+        await dbContext.Pets.AddAsync(pet);
+        await dbContext.SaveChangesAsync();
+
+        return CreatedAtRoute(nameof(GetById), new { id = pet.Id }, newPet);
+    }
+}
+
+public record NewPet(string Name, int Age, int BreedId)
+{
+    public Pet ToPet()
+    {
+        return new Pet() { Name = Name, Age = Age, BreedId = BreedId };
     }
 }
