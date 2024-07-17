eea891be0ff47f43883a04433646cee065500f61
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-07-17 14:21:09 -0700

    02_03e

---
+++ src/Wpm.Management.Api/DataAccess/ManagementDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Wpm.Management.Api.DataAccess;
+
+public class ManagementDbContext(DbContextOptions<ManagementDbContext> options) : DbContext(options)
+{
+    public DbSet<Pet> Pets { get; set; }
+    public DbSet<Breed> Breeds { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Breed>().HasData(
+            [
+                new Breed(1, "Beagle"),
+                new Breed(2, "Staffordshire Terrier")
+            ]
+        );
+        modelBuilder.Entity<Pet>().HasData
+        (
+            [
+                new Pet(){ Id = 1, Name = "Gianni", Age = 13, BreedId = 1},
+                new Pet(){ Id = 2, Name = "Nina", Age = 10, BreedId = 1},
+                new Pet(){ Id = 3, Name = "Cati", Age = 12, BreedId = 2},
+            ]
+        );
+    }
+}
+
+public static class ManagementDbContextExtensions
+{
+    public static void EnsureDbIsCreated(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var context = scope.ServiceProvider.GetService<ManagementDbContext>();
+        context!.Database.EnsureCreated();
+    }
+}
+
+public class Pet
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int BreedId { get; set; }
+    public Breed Breed { get; set; }
+}
+
+public record Breed(int Id, string Name);
--- src/Wpm.Management.Api/Program.cs
+++ src/Wpm.Management.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Wpm.Management.Api.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ManagementDbContext>(options =>
+{
+    options.UseInMemoryDatabase("WpmManagement");
+});
 
 var app = builder.Build();
+
+app.EnsureDbIsCreated();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
